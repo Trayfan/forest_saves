b52ebd18acd9698f2b51ce84f9254437172e4b5c
Alex <test@test.ru>
2023-02-25 15:33:34 +0400

    test

---
+++ upload.bat
@@ -0,0 +1,7 @@
+git init
+git remote add origin https://github.com/Trayfan/forest_saves.git
+git config --global user.email "test@test.ru"
+git config --global user.name "Alex"
+git add . 
+git commit -m "test"
+git push -u origin main
